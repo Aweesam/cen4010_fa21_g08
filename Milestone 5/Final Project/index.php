3e5715d41ba92aecabbbb19024c767a2c92e3e31
Vanc8262 <86838787+Vanc8262@users.noreply.github.com>
2021-11-24 20:47:56 -0500

    Update index.php

--- Milestone 5/Final Project/index.php
+++ Milestone 5/Final Project/index.php
@@ -81,6 +81,16 @@ if(isset($_POST['register']))
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-DCNNNH799N"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-DCNNNH799N');
+    </script>
+    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

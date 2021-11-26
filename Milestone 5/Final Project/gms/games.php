8a71acd8830bbb034120c0a14e65a2fd5f107ab0
Ach1207 <89668988+Ach1207@users.noreply.github.com>
2021-11-26 18:15:39 -0500

    Update games.php

--- Milestone 5/Final Project/gms/games.php
+++ Milestone 5/Final Project/gms/games.php
@@ -10,8 +10,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Days+One" />
 
-    <script type="text/javascript"src="js/JQuery.js"></script>
-    <script type="text/javascript" src="js/lightslider.js"></script>  
+    <script type="text/javascript"src="gms/js/JQuery.js"></script>
+    <script type="text/javascript" src="gms/js/lightslider.js"></script>  
 
 </head>
 <body>

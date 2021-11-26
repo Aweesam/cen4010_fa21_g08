bcb47438007c05dd22a05ff10a4532bb673b6d5d
Ach1207 <89668988+Ach1207@users.noreply.github.com>
2021-11-26 18:39:06 -0500

    games fix

--- Milestone 5/Final Project/gms/games.php
+++ Milestone 5/Final Project/gms/games.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="utf-8">
     <title>Games tab</title>
-    <link rel="stylesheet" type="text/css" href="gms/css/games_Styles.css">
-    <link rel="stylesheet" type="text/css" href="gms/css/lightslider.css">
-    <link rel="stylesheet" href="gms/css/main.css">
+    <link rel="stylesheet" type="text/css" href="games_Styles.css">
+    <link rel="stylesheet" type="text/css" href="lightslider.css">
+    <link rel="stylesheet" href="css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Days+One" />
 
-    <script type="text/javascript"src="gms/js/JQuery.js"></script>
-    <script type="text/javascript" src="gms/js/lightslider.js"></script>  
+    <script type="text/javascript"src="JQuery.js"></script>
+    <script type="text/javascript" src="lightslider.js"></script>  
 
 </head>
 <body>
@@ -138,7 +138,7 @@
 
 
 
-<script type="text/javascript" src="gms/js/script.js"></script>
+<script type="text/javascript" src="script.js"></script>
 </body>
 
 
---
+++ Milestone 5/Final Project/gms/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+});
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}

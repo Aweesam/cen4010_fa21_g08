2937442e814717acbcc749fc18dc57d0ca4cb913
Ach1207 <89668988+Ach1207@users.noreply.github.com>
2021-11-26 17:35:36 -0500

    updated games page

--- Milestone 5/Final Project/gms/games.php
+++ Milestone 5/Final Project/gms/games.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="utf-8">
     <title>Games tab</title>
-    <link rel="stylesheet" type="text/css" href="css/games_Styles.css">
-    <link rel="stylesheet" type="text/css" href="css/lightslider.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="gms/css/games_Styles.css">
+    <link rel="stylesheet" type="text/css" href="gms/css/lightslider.css">
+    <link rel="stylesheet" href="gms/css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Days+One" />
@@ -23,9 +23,9 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="#">Forum</a></li>
+                    <li class="nav-item"><a class="nav-link" href="forum/index.php">Forum</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Gallery</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Games</a></li>
+                        <li class="nav-item"><a class="nav-link" href="gms/games.php">Games</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Resources</a></li>
                     </ul>
                     <ul class="navbar-nav ms-auto">
@@ -138,7 +138,7 @@
 
 
 
-<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="gms/js/script.js"></script>
 </body>
 
 

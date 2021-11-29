2aa77b82d3beebe52585a9cdb5a7667212a1ddc2
Sam <syambo2020@fau.edu>
2021-11-29 13:59:32 -0500

    Update games.php
    Fixed css page linking

--- Milestone 5/Final Project/gms/games.php
+++ Milestone 5/Final Project/gms/games.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8">
     <title>Games tab</title>
-    <link rel="stylesheet" type="text/css" href="games_Styles.css">
-    <link rel="stylesheet" type="text/css" href="lightslider.css">
+    <link rel="stylesheet" type="text/css" href="css/games_Styles.css">
+    <link rel="stylesheet" type="text/css" href="css/lightslider.css">
     <link rel="stylesheet" href="css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>

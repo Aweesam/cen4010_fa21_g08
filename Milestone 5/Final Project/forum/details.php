acec448694f2da251a585bb7e28b29196342c473
grodriguez2017 <grodriguez2017@fau.edu>
2021-11-29 15:38:15 -0500

    fix css paths

--- Milestone 5/Final Project/forum/details.php
+++ Milestone 5/Final Project/forum/details.php
@@ -177,7 +177,7 @@ else
             <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
                 <li class="nav-item active"><a class="nav-link" href="../forum/index.php">Forum</a></li>
                 <li class="nav-item"><a class="nav-link" href="#!">Gallery</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Games</a></li>
+                <li class="nav-item"><a class="nav-link" href="../gms/games.php">Games</a></li>
                 <li class="nav-item"><a class="nav-link" href="#!">Resources</a></li>
             </ul>
             <ul class="navbar-nav ms-auto">
--- Milestone 5/Final Project/gms/games.php
+++ Milestone 5/Final Project/gms/games.php
@@ -1,17 +1,33 @@
+<?php
+require_once "../php/db_connect.php";
+require_once "../php/functions.php";
+
+session_start();
+
+if(isset($_SESSION['username'])){
+    $login_logout = '<a href="../php/logout.php" target="_parent"><button type="button" class="btn btn-primary">Logout</button></ul></li></a>'; 
+}
+
+else
+{
+    $login_logout = '<a href="../Signup/signin.php" target="_parent"><button type="button" class="btn btn-primary">Login</button></ul></li></a>';
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <title>Games tab</title>
-    <link rel="stylesheet" type="text/css" href="css/games_Styles.css">
-    <link rel="stylesheet" type="text/css" href="css/lightslider.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="../gms/css/games_Styles.css">
+    <link rel="stylesheet" type="text/css" href="../gms/css/lightslider.css">
+    <link rel="stylesheet" type="text/css" href="../gms/css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Days+One" />
 
-    <script type="text/javascript"src="JQuery.js"></script>
-    <script type="text/javascript" src="lightslider.js"></script>  
+    <script type="text/javascript"src="../gms/js/JQuery.js"></script>
+    <script type="text/javascript" src="../gms/js/lightslider.js"></script>  
 
 </head>
 <body>
@@ -19,18 +35,20 @@
     <div class="holder">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="#!"><img src="img/New Project.png" alt="CC Pic" style="width:52px;height:52px;"></a>
+                <a class="navbar-brand" href="../index.php"><img src="img/New Project.png" alt="CC Pic" style="width:52px;height:52px;"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="forum/index.php">Forum</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../forum/index.php">Forum</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Gallery</a></li>
-                        <li class="nav-item"><a class="nav-link" href="gms/games.php">Games</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../gms/games.php">Games</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Resources</a></li>
                     </ul>
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <button type="button" class="btn btn-primary">Log In/Log Out</button></ul>/li>
+                        <?php
+                            echo $login_logout.PHP_EOL;
+                        ?>
                         </li>
                     </ul>
                 </div>
@@ -138,7 +156,7 @@
 
 
 
-<script type="text/javascript" src="script.js"></script>
+<script type="text/javascript" src="../gms/js/script.js"></script>
 </body>
 
 

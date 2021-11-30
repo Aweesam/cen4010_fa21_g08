c9d026345ae6615970730e1d8ad0bc1e26f3ec2c
Ach1207 <89668988+Ach1207@users.noreply.github.com>
2021-11-30 18:56:10 -0500

    update

--- Milestone 5/Final Project/gms/games.php
+++ Milestone 5/Final Project/gms/games.php
@@ -19,7 +19,7 @@ else
 <head>
     <meta charset="utf-8">
     <title>Games tab</title>
-    <link rel="stylesheet" type="text/css" href="../gms/css/games_Styles.css">
+    <link rel="stylesheet" type="text/css" href="../gms/css/gamesstyles.css">
     <link rel="stylesheet" type="text/css" href="../gms/css/lightslider.css">
     <link rel="stylesheet" type="text/css" href="../gms/css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">

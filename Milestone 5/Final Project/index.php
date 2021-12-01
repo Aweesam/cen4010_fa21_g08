3439bf86ef8116a6b904069d2e3a3d29d4af4b22
Samuel Adkins <adkinss2020@fau.edu>
2021-12-01 13:42:38 -0500

    Nav Links Forum and homepage
    Updated Nav for homepage and forums

--- Milestone 5/Final Project/index.php
+++ Milestone 5/Final Project/index.php
@@ -109,6 +109,7 @@ if(isset($_POST['register']))
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="profile/index.html">Home</a></li>
                         <li class="nav-item"><a class="nav-link" href="forum/index.php">Forum</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Gallery</a></li>
                         <li class="nav-item"><a class="nav-link" href="gms/games.html">Games</a></li>

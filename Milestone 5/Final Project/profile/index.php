e61f9e53ef340b2647ef7753dc97beff69e4c945
Samuel Adkins <adkinss2020@fau.edu>
2021-12-06 13:59:34 -0500

    Profile update
    edit to empty boxes

--- Milestone 5/Final Project/profile/index.php
+++ Milestone 5/Final Project/profile/index.php
@@ -121,69 +121,36 @@ $results_profile->close();
             <iframe src="https://public.domo.com/cards/YE040" width="700" height="450" marginheight="0" marginwidth="0" frameborder="0"></iframe>
           </div>
         </div>
-        <!-- Nested row for non-featured blog posts-->
-        <div class="row">
+         <!-- Nested row for non-featured blog posts-->
+         <div class="row">
           <div class="col-lg-6">
             <!-- Blog post-->
-            <div class="card mb-4">
-              <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
-                  alt="..." /></a>
-              <div class="card-body">
-                <div class="small text-muted">January 3, 2021</div>
-                <h2 class="card-title h4">Post Title</h2>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque,
-                  nulla.</p>
-                <a class="btn btn-primary" href="#!">Read more →</a>
-              </div>
+            <div class="card mb-4" id="row1">
+              <iframe src="https://www.health.gov/myhealthfinder?widget=true" 
+              name="myhealthfinderframe" 
+              frameborder="0" id="myhealthfinderframe"
+               scrolling="yes" height="100%" width="100%" 
+               marginheight="0" title="myhealthfinder widget"
+                marginwidth="0">
+                <p>Your browser does not support iframes.</p></iframe>
             </div>
             <!-- Blog post-->
-            <div class="card mb-4">
-              <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
-                  alt="..." /></a>
-              <div class="card-body">
-                <div class="small text-muted">January 2, 2021</div>
-                <h2 class="card-title h4">Post Title</h2>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque,
-                  nulla.</p>
-                <a class="btn btn-primary" href="#!">Read more →</a>
-              </div>
+            <div class="card mb-4" style="width:700px; background-color: khaki; height:800px"> 
+              <script src='https://redditjs.com/subreddit.js' data-subreddit='COVID19' data-sort='hot' data-theme='dark' data-height='800' data-width='700'></script>
             </div>
           </div>
           <div class="col-lg-6">
             <!-- Blog post-->
-            <div class="card mb-4" style="width: 20rem; background-color: antiquewhite;" >
+            <div class="card mb-4" style="width: 20rem; background-color: antiquewhite; height:600px;" >
               <div class="card-body">
                 <div data-mediatype='widget' data-mediaid='426456' data-apiroot='tools.cdc.gov/api' data-cdc-widget='syndicationIframe'></div><script src='https://www.cdc.gov/TemplatePackage/contrib/widgets/tp-widget-external-loader.js' ></script><noscript>You need javascript enabled to view this content or go to <a href='https://tools.cdc.gov/api/v2/resources/media/426456/noscript'>source URL</a>.</noscript>
               </div>
             </div>
-            <!-- Blog post-->
-            <div class="card mb-4">
-              <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
-                  alt="..." /></a>
-              <div class="card-body">
-                <div class="small text-muted">January 1, 2021</div>
-                <h2 class="card-title h4">Post Title</h2>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque,
-                  nulla? Quos cum ex quis soluta, a laboriosam.</p>
-                <a class="btn btn-primary" href="#!">Read more →</a>
-              </div>
-            </div>
+            
+            
           </div>
         </div>
-        <!-- Pagination-->
-        <nav aria-label="Pagination">
-          <hr class="my-0" />
-          <ul class="pagination justify-content-center my-4">
-            <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true">Newer</a>
-            </li>
-            <li class="page-item active" aria-current="page"><a class="page-link" href="#!">1</a></li>
-            <li class="page-item"><a class="page-link" href="#!">2</a></li>
-            <li class="page-item"><a class="page-link" href="#!">3</a></li>
-            <li class="page-item disabled"><a class="page-link" href="#!">...</a></li>
-            <li class="page-item"><a class="page-link" href="#!">15</a></li>
-            <li class="page-item"><a class="page-link" href="#!">Older</a></li>
-          </ul>
-        </nav>
+        
       </div>
       <!-- Side widgets-->
       <div class="col-6 col-md-4">
